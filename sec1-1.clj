158cab345e1ed26e2423208c0f2892b3cfc1737f
Falko <falko@mb.local>
2009-05-28 23:12:36 +0200

    up to 1.26

--- sec1-1.clj
+++ sec1-1.clj
@@ -3,6 +3,42 @@
 
 
 
+;; Ex. 1.1
+10                              ; => 10
+(+ 5 3 4)                       ; => 12
+(- 9 1)                         ; => 8
+(/ 6 2)                         ; => 3
+(+ (* 2 4) (- 4 6))             ; => 6
+(def a 3)
+(def b (+ a 1))
+(+ a b (* a b))                 ; => 19
+(= a b)                         ; => false
+(if (and (> b a) (< b (* a b)))
+  b
+  a)                            ; => 4
+(cond
+  (= a 4) 6
+  (= b 4) (+ 7 6 a)
+  :else -1)                     ; => 16
+(+ 2 (if (> b a) b a))          ; => 6
+(* (cond (> a b) a
+         (< a b) b
+         :else -1)
+   (+ a 1))                     ; => 16
+
+
+
+;; Ex. 1.2
+(/ (+ 5
+      4
+      (- 2 (- 3 (+ 6 4/5))))
+   (* 3
+      (- 6 2)
+      (- 2 7)))
+; => -37/150
+
+
+
 ;; Ex. 1.3
 ;  Maybe using SORT is not really the point of this exercise ;-)
 (defn ex-1-3 [a b c]
@@ -81,7 +117,7 @@
 ; no possible values in between. This can cause the IMPROVE function to
 ; return it's GUESS argument, unimproved. Example:
 ; (improve 3.1464265445104545E50 99E99) returns 3.1464265445104545E50
-; (yes those two numbers are the same ;-)
+; (yes, those two numbers are the same ;-).
 ;
 ; As the authors suggest, SQRT-ITER can be improved by testing for a small
 ; ratio between the guess and the improved guess. This way, for small
